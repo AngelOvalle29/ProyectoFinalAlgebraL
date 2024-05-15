98df3e5c6b0129e6b0cb9c0edfcb0bc728a28ad8
Angel Ovalle <angelitoovalle10barsa@gmail.com>
2024-05-14 18:43:09 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinalAlgebra/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ProyectoFinalAlgebra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxCarnet = new System.Windows.Forms.TextBox();
+            this.TextBoxResultado = new System.Windows.Forms.TextBox();
+            this.SwitchGenerar = new System.Windows.Forms.Button();
+            this.SwitchLimpiar = new System.Windows.Forms.Button();
+            this.SwitchGuardar = new System.Windows.Forms.Button();
+            this.ImagenPic = new System.Windows.Forms.PictureBox();
+            this.QR = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(82, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar Número de Carnet";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Linen;
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(718, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nuevo Carnet";
+            // 
+            // TextBoxCarnet
+            // 
+            this.TextBoxCarnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.TextBoxCarnet.Location = new System.Drawing.Point(38, 100);
+            this.TextBoxCarnet.Multiline = true;
+            this.TextBoxCarnet.Name = "TextBoxCarnet";
+            this.TextBoxCarnet.Size = new System.Drawing.Size(499, 41);
+            this.TextBoxCarnet.TabIndex = 2;
+            // 
+            // TextBoxResultado
+            // 
+            this.TextBoxResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.TextBoxResultado.Location = new System.Drawing.Point(639, 100);
+            this.TextBoxResultado.Multiline = true;
+            this.TextBoxResultado.Name = "TextBoxResultado";
+            this.TextBoxResultado.Size = new System.Drawing.Size(382, 41);
+            this.TextBoxResultado.TabIndex = 3;
+            this.TextBoxResultado.Visible = false;
+            this.TextBoxResultado.TextChanged += new System.EventHandler(this.TextBoxResultado_TextChanged);
+            // 
+            // SwitchGenerar
+            // 
+            this.SwitchGenerar.BackColor = System.Drawing.Color.Snow;
+            this.SwitchGenerar.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SwitchGenerar.Location = new System.Drawing.Point(639, 166);
+            this.SwitchGenerar.Name = "SwitchGenerar";
+            this.SwitchGenerar.Size = new System.Drawing.Size(382, 84);
+            this.SwitchGenerar.TabIndex = 4;
+            this.SwitchGenerar.Text = "Generar Nuevo Carnet y Codigo QR para Escanear";
+            this.SwitchGenerar.UseVisualStyleBackColor = false;
+            this.SwitchGenerar.Click += new System.EventHandler(this.SwitchGenerar_Click);
+            // 
+            // SwitchLimpiar
+            // 
+            this.SwitchLimpiar.BackColor = System.Drawing.Color.Snow;
+            this.SwitchLimpiar.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SwitchLimpiar.Location = new System.Drawing.Point(639, 284);
+            this.SwitchLimpiar.Name = "SwitchLimpiar";
+            this.SwitchLimpiar.Size = new System.Drawing.Size(184, 104);
+            this.SwitchLimpiar.TabIndex = 5;
+            this.SwitchLimpiar.Text = "Limpiar";
+            this.SwitchLimpiar.UseVisualStyleBackColor = false;
+            this.SwitchLimpiar.Click += new System.EventHandler(this.SwitchLimpiar_Click);
+            // 
+            // SwitchGuardar
+            // 
+            this.SwitchGuardar.BackColor = System.Drawing.Color.Snow;
+            this.SwitchGuardar.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SwitchGuardar.Location = new System.Drawing.Point(847, 284);
+            this.SwitchGuardar.Name = "SwitchGuardar";
+            this.SwitchGuardar.Size = new System.Drawing.Size(174, 104);
+            this.SwitchGuardar.TabIndex = 6;
+            this.SwitchGuardar.Text = "Guardar Carnet";
+            this.SwitchGuardar.UseVisualStyleBackColor = false;
+            this.SwitchGuardar.Click += new System.EventHandler(this.SwitchGuardar_Click);
+            // 
+            // ImagenPic
+            // 
+            this.ImagenPic.Location = new System.Drawing.Point(674, 491);
+            this.ImagenPic.Name = "ImagenPic";
+            this.ImagenPic.Size = new System.Drawing.Size(100, 50);
+            this.ImagenPic.TabIndex = 7;
+            this.ImagenPic.TabStop = false;
+            this.ImagenPic.Click += new System.EventHandler(this.ImagenPic_Click);
+            // 
+            // QR
+            // 
+            this.QR.Location = new System.Drawing.Point(38, 177);
+            this.QR.Name = "QR";
+            this.QR.Size = new System.Drawing.Size(499, 416);
+            this.QR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.QR.TabIndex = 8;
+            this.QR.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(1072, 656);
+            this.Controls.Add(this.QR);
+            this.Controls.Add(this.ImagenPic);
+            this.Controls.Add(this.SwitchGuardar);
+            this.Controls.Add(this.SwitchLimpiar);
+            this.Controls.Add(this.SwitchGenerar);
+            this.Controls.Add(this.TextBoxResultado);
+            this.Controls.Add(this.TextBoxCarnet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxCarnet;
+        private System.Windows.Forms.TextBox TextBoxResultado;
+        private System.Windows.Forms.Button SwitchGenerar;
+        private System.Windows.Forms.Button SwitchLimpiar;
+        private System.Windows.Forms.Button SwitchGuardar;
+        private System.Windows.Forms.PictureBox ImagenPic;
+        private System.Windows.Forms.PictureBox QR;
+    }
+}
+
---
+++ ProyectoFinalAlgebra/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using QRCoder;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection.Emit;
+using System.Windows.Controls;
+
+
+namespace ProyectoFinalAlgebra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SwitchLimpiar_Click(object sender, EventArgs e)
+        {
+            //Instrucciones para el botón de limpiar los datos tanto de los textBox como del PictureBox del QR
+            TextBoxCarnet.Text = "";
+            TextBoxResultado.Text = "";
+            QR.Image = null;
+        }
+
+        private void PropiedadesCodigoQR(string texto)
+        {
+            //Codigo el cual habilita la opción de generar un Qr para la compilación
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(texto, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            // Instrucción para el tamaño del QR
+            QR.Image = qrCode.GetGraphic(5);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextBoxResultado.Visible = false;
+            label2.Visible = false;
+        }
+        private string NuevoCarnet(string NewLicense)
+        {
+            //Declaración de la matriz la cual hace referencia al Z11
+            int[] Z11 = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            //Condición la cual evalúa el caso de que el usuario no ingrese la cantidad necesaria de dígitos
+            if (NewLicense.Length != 9)
+            {
+                MessageBox.Show("Favor de ingresar la cantidad exacta de 9 dígitos", "Precaución", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return "";
+            }
+
+            //Instrucción que genera los valores del carnet nuevo y que evalúa que solo contenga dígitos la cadena 
+            int[] digitos = new int[9];
+            for (int i = 0; i < 9; i++)
+            {
+                if (!int.TryParse(NewLicense[i].ToString(), out digitos[i]))
+                {
+                    MessageBox.Show("El carnet solo admite datos numéricos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return "";
+                }
+            }
+            //Procedimiento matemático para generar el último carácter el cual es el de verificación
+            int suma = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                suma += digitos[i] * Z11[i];
+            }
+
+            int Verificacion = (11 - (suma % 11)) % 11;
+
+            // Instrucción con el fin de ocultar el último dígito de el carnet
+            char letradeVerificacion = (char)(Verificacion + 65); // Conversión del Número obtenido a una letra mayúscula
+            return NewLicense + letradeVerificacion;
+        }
+        private void GuardarDatos(string carneNuevo )
+        {
+            // Función e instrucciones las cuales estipulan la dirección a donde serán guardados los datos
+            // Y genera el archivo .txt el cual contendrá todos los carnets ingresados
+            string direccionArchivo = "Datos.txt";
+            using (StreamWriter sw = File.AppendText(direccionArchivo)) // Hace que no se sobreescriba el contenido
+            {
+                sw.WriteLine(carneNuevo);
+            }
+        }
+        private void SwitchGuardar_Click(object sender, EventArgs e)
+        {
+            //Botón con la funcionalidad de guardar los carnets en el archivo .txt
+            string NuevoCarnet = TextBoxResultado.Text;
+            if (!string.IsNullOrEmpty(NuevoCarnet))
+            {
+                GuardarDatos(NuevoCarnet);
+                MessageBox.Show("El carnet generado se ha guardado con éxito!!!", "Notificación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No existe algún carnet el cual se pueda guardar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void SwitchGenerar_Click(object sender, EventArgs e)
+        {
+            //Botón el cual genera el nuevo carnet y el QR
+            TextBoxResultado.Visible = true;
+            label2.Visible = true;
+            string carneActual = TextBoxCarnet.Text;
+            string nuevoCarne = NuevoCarnet(carneActual);
+            TextBoxResultado.Text = nuevoCarne;
+            PropiedadesCodigoQR(nuevoCarne);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImagenPic_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+   
